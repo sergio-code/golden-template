511771621e3cf36e0275601438e37339ee164881
mrgromov <mailrxtx@gmail.com>
2019-02-25 00:58:25 +0400

    Script for responsive menu

--- src/index.js
+++ src/index.js
@@ -1,2 +1,36 @@
 import "./styles/main.scss"
-console.info("Page is loaded!")
+
+document.addEventListener("DOMContentLoaded", function() {
+    const menuCloseBtnElement = document.createElement("a")
+    menuCloseBtnElement.setAttribute("href", "javascript:void(0)")
+    menuCloseBtnElement.setAttribute("class", "nav__close")
+    menuCloseBtnElement.style.display = "none"
+    menuCloseBtnElement.innerHTML = "&times;"
+    menuCloseBtnElement.onclick = () => {
+        document.querySelector(".nav__close").style.display = "none"
+        document.querySelector(".nav").style.display = "none"
+    }
+    document.querySelector(".nav").appendChild(menuCloseBtnElement)
+
+    const burgerBtnElement = document.createElement("a")
+    burgerBtnElement.setAttribute("href", "javascript:void(0)")
+    burgerBtnElement.setAttribute("class", "burger_btn nav__open")
+    burgerBtnElement.innerHTML = "&#9776;"
+    burgerBtnElement.onclick = () => {
+        document.querySelector(".nav__close").style.display = "block"
+        document.querySelector(".nav").style.display = "block"
+    }
+    document.querySelector(".heading").appendChild(burgerBtnElement)
+
+    //resize fix
+    window.onresize = () => {
+        console.log("resized")
+        if (window.innerWidth <= 1024) {
+            document.querySelector(".nav__close").style.display = "none"
+            document.querySelector(".nav").style.display = "none"
+        } else {
+            document.querySelector(".nav__close").style.display = "none"
+            document.querySelector(".nav").style.display = "block"
+        }
+    }
+})
